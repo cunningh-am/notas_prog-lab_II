1cfaa3ac1bd9cf0c375325507a619994c2881cf4
valentin <jclyffe@DESKTOP-6EK9PFG.localdomain>
2022-03-29 19:02:28 -0300

    ejercicio de la clase

---
+++ clases/clase_29-3/MiClase.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace clase_29_3
+{
+    class MiClase
+    {
+        #region atributos
+
+        public static int estatico;
+        public int no_estatico;
+
+        #endregion
+
+        #region metodos
+        public static void MostrarEstatico()
+        {
+            Console.WriteLine(MiClase.estatico);
+        }
+
+        public void MostrarNoEstatico()
+        {
+            Console.WriteLine(this.no_estatico);
+        }
+
+        public void MostrarTodos()
+        {
+            MiClase.MostrarEstatico();
+            this.MostrarNoEstatico();
+        }
+
+        #endregion
+
+        #region constructor
+        #region de instancia (por defecto)
+
+        //public MiClase()
+        //{
+        //    this.no_estatico = 5;
+        //}
+
+        #endregion
+        #region de instancia (con parametro)
+
+        public MiClase()
+        {
+            this.no_estatico = 5;
+        }
+
+        #endregion
+        #region de clase (static)
+
+        static MiClase()
+        {
+            MiClase.estatico = 2;
+        }
+
+        #endregion
+        #endregion
+    }
+}
--- clases/clase_29-3/Program.cs
+++ clases/clase_29-3/Program.cs
@@ -2,37 +2,6 @@
 
 namespace clase_29_3
 {
-    class MiClase
-    {
-        public static int estatico;
-        public int no_estatico;
-
-        public static void MostrarEstatico()
-        {
-            Console.WriteLine(MiClase.estatico);
-        }
-
-        public void MostrarNoEstatico()
-        {
-            Console.WriteLine(this.no_estatico);
-        }
-
-        public void MostrarTodos()
-        {
-            MiClase.MostrarEstatico();
-            this.MostrarNoEstatico();
-        }
-
-        public MiClase()
-        {
-            this.no_estatico = 5;
-        }
-
-        static MiClase()
-        {
-            MiClase.estatico = 2;
-        }
-    }
     class Program
     {
         static void Main(string[] args)
