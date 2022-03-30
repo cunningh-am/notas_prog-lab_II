ad6ef3b5e1930064a81fbe368d85df9a93c1fe50
valentin <jclyffe@DESKTOP-6EK9PFG.localdomain>
2022-03-30 16:49:30 -0300

    ejercicios 8 y 9

---
+++ ejercicios/ejercicio-l09/Program.cs
@@ -0,0 +1,36 @@
+﻿/*Escribir un programa que imprima por consola un triángulo
+ * El usuario indicará cuál será la altura del triángulo ingresando un número entero positivo.*/
+
+using System;
+
+namespace ejercicio_l09
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool checkInt;
+            int cantidad;
+
+            do
+            {
+                Console.Write("Ingrese la altura del triangulo: ");
+                checkInt = int.TryParse(Console.ReadLine(), out cantidad);
+            } while (!checkInt);
+
+            for (int i = 1; i <= cantidad; i++)
+            {
+                for (int j = 0; j <= cantidad - i ; j++)
+                {
+                    Console.Write(" ");
+                }
+
+                for (int k = 1; k <= i * 2 - 1; k++)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
