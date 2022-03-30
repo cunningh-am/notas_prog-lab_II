8ceb5c89806977c9cca450c026db85c654d43e49
valentin <jclyffe@DESKTOP-6EK9PFG.localdomain>
2022-03-30 11:03:15 -0300

    Agrego clase 30-3

---
+++ clases/clase_30-3/Cosa.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace clase_30_3
+{
+    class Cosa
+    {
+        private int entero;
+        private string cadena;
+        private DateTime date;
+
+        public void EstablecerValor(int x)
+        {
+            this.entero = x;
+        }
+
+        public void EstablecerValor(string x)
+        {
+            this.cadena = x;
+        }
+
+        public void EstablecerValor(DateTime x)
+        {
+            this.date = x;
+        }
+
+        public static string Mostrar(Cosa obj)
+        {
+            return obj.Mostrar();
+        }
+
+        private string Mostrar()
+        {
+            return "Entero: " + this.entero + ", Cadena: " + this.cadena + ", Fecha: " + this.date.ToShortDateString();
+        }
+    }
+}
---
+++ clases/clase_30-3/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace clase_30_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Cosa obj = new Cosa();
+
+            obj.EstablecerValor(10);
+            obj.EstablecerValor(DateTime.Now);
+            obj.EstablecerValor("Hola!");
+
+            Console.WriteLine(Cosa.Mostrar(obj));
+        }
+    }
+}
